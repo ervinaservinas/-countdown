de5b35245e0c45b6f2b3d0051787ea3c4de52a1b
Ervinas <84942742+ervinaservinas@users.noreply.github.com>
2022-06-01 19:44:23 +0300

    changed js code to working one

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const newYears = '1 Jan 2023'
 
 function countdown(){
-    const newYearsDate = new Date(newYears)
+    const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) / 1000
+const totalSeconds= (newYearsDate - currentDate)   / 1000; 
 
-    const days = Math.floor(seconds/3600/24)
-    console.log(days)
+
+
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const minutes = Math.floor(totalSeconds/60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    console.log(days);
 }
 // call the timer function
 
